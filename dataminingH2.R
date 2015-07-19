5d612c753b79b97bf650a8283be5300c33352751
shengreen <shengreen@hotmail.com>
2015-07-19 20:52:17 +0800

    Update and rename datamainingH2.R to dataminingH2.R

--- dataminingH2.R
+++ dataminingH2.R
@@ -13,14 +13,19 @@ y1 <- scale(y, center=TRUE, scale=FALSE)
 beta_hat1 <- solve(var(x1))%*%cor(x1, y1)
 temp <- apply(x1, 2, mean)
 beta0_hat1 <- mean(y1)-t(temp)%*%beta_hat1
+eps_var1 <- var(beta_hat1%*%x1 + beta0_hat1 - y1)
+plot(x1%*%beta_hat1 + beta0_hat1 - y1)
 
 #############################################
 #
 #############################################
 x2 <- cbind(array(rep(1), dim=c(n,1)), x)
 beta_hat2 <- solve(t(x2)%*%x2)%*%(t(x2)%*%y)
+eps_var2 <- var(x2%*%beta_hat2 - y)
+plot(x2%*%beta_hat2 - y)
 
 #############################################
 #
 #############################################
 lm.fit <- lm(y~x)
+eps_var3 <- lm.fit.
