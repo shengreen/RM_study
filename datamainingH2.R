202dcaf4ff7b053bef186f4dcf720b43c98814da
shengreen <shengreen@hotmail.com>
2015-07-19 17:39:47 +0800

    Update and rename datamaining_H2.R to datamainingH2.R

---
+++ datamainingH2.R
@@ -0,0 +1,26 @@
+rm(list = ls());
+n = 30;
+varnum = 10;
+x <- array(rnorm(n*varnum), dim=c(n, varnum));
+beta <- array(c(1, 0.8, 0.6, 0.4, 0.2, 0, 0, 0, 0, 0), dim=c(varnum, 1))
+y <- x%*%beta + array(rep(rnorm(n), 1), dim=c(n, 1))
+
+#############################################
+#
+#############################################
+x1 <- scale(x, center=TRUE, scale=FALSE)
+y1 <- scale(y, center=TRUE, scale=FALSE)
+beta_hat1 <- solve(var(x1))%*%cor(x1, y1)
+temp <- apply(x1, 2, mean)
+beta0_hat1 <- mean(y1)-t(temp)%*%beta_hat1
+
+#############################################
+#
+#############################################
+x2 <- cbind(array(rep(1), dim=c(n,1)), x)
+beta_hat2 <- solve(t(x2)%*%x2)%*%(t(x2)%*%y)
+
+#############################################
+#
+#############################################
+lm.fit <- lm(y~x)
